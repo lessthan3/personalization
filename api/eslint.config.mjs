8e51947ad5395b5ae1b308f4b0193b6ebce569e8
Miguel Murillo <miguel.murillo@globant.com>
2024-12-04 16:10:00 -0600

    Adding eslint config

---
+++ api/eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    languageOptions: { globals: globals.node },
+    plugins: {
+      prettier: pluginPrettier
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier
+];
