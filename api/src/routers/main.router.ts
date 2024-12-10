fc12d9f10c3221c5e9431033a3c305549bd78f20
infantea <137831141+infantea@users.noreply.github.com>
2024-12-10 14:42:00 -0300

    [Add Health Endpoint] - Created health.controller.ts - Created health.router.ts - Upddate main.router.ts

--- api/src/routers/main.router.ts
+++ api/src/routers/main.router.ts
@@ -1,11 +1,15 @@
 import express, { type Router } from "express";
 import { authHandler } from "../middleware/auth.middleware";
+import { healthRouter } from "./health.router";
 
 // For reference(REST API Guide):
 // https://www.restapitutorial.com/
 // https://www.restapitutorial.com/lessons/restfulresourcenaming.html
 // https://www.restapitutorial.com/lessons/httpmethods.html
 
-const router: Router = express.Router().use(authHandler);
-
+const router: Router = express
+  .Router()
+  .use("/health", healthRouter)
+  .use(authHandler)
+  
 export { router as mainRouter };
